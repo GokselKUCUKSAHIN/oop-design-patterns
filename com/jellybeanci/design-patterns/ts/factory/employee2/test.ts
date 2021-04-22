0ef639bed520895129be815d3d33d8e71a2b1978
jellybeanci <gokselkucuksahin@gmail.com>
2021-04-22 15:50:16 +0300

    FM2 Complete.

---
+++ com/jellybeanci/design-patterns/ts/factory/employee2/test.ts
@@ -0,0 +1,38 @@
+import {HR} from "./hr";
+import {PayrollOffice} from "./payroll-office";
+import {EmployeeFactory} from "./employee-factory";
+import {ManagerFactory} from "./manager-factory";
+import {DirectorFactory} from "./director-factory";
+
+class Test {
+  public static main = () => {
+    const hr = new HR();
+    const po = new PayrollOffice();
+
+    const employeeFactory = new EmployeeFactory();
+    const managerFactory = new ManagerFactory();
+    const directorFactory = new DirectorFactory();
+
+    // Add more employees
+    hr.addNewEmployee(employeeFactory.create("Ali"));
+    hr.addNewEmployee(employeeFactory.create("Ayşe"));
+    hr.addNewEmployee(employeeFactory.create("Veli"));
+    hr.addNewEmployee(managerFactory.create("Ece"));
+    hr.addNewEmployee(managerFactory.create("Ahmet"));
+    hr.addNewEmployee(directorFactory.create("Göksel"));
+
+    hr.listEmployees();
+
+    console.log("~~~~");
+
+    // Now pay time!
+    const employees = hr.getEmployees();
+    employees.forEach(employee => {
+      po.paySalary(employee);
+    });
+    employees.forEach(employees => {
+      employees.printInfo();
+    });
+  };
+}
+Test.main();
