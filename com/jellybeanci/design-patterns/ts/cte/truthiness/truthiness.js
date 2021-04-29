8b7c1311f3f05c0b870c37e2f142acfb154c034e
Jellybeanci <gokselkucuksahin@gmail.com>
2021-04-29 04:26:32 +0300

    Builder new examples added.

--- com/jellybeanci/design-patterns/ts/cte/truthiness/truthiness.js
+++ com/jellybeanci/design-patterns/ts/cte/truthiness/truthiness.js
@@ -3,5 +3,5 @@ const greet = (msg) => console.log(msg);
 const statament = false;
 const otherStatement = true;
 
-statament && greet("false");
-otherStatement && greet("true");
+!! statament && greet("false");
+!! otherStatement && greet("true");
